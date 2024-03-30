73d63c965d9d542513e98696211a5ba4d829dd7b
Marcos Veniciu de sa Barbalho <marcos.barbalho@ufv.br>
2024-03-30 13:03:19 -0300

    Erro #1
    tentativa 17

--- main.py
+++ main.py
@@ -481,7 +481,7 @@ if __name__ == "__main__":
 
     parser = get_parser()
     parser = parser.parse_args()
-    trainer = Trainer.add_argparse_args(parser)#accelerator= "gpu")
+    parser = Trainer()
 
     opt, unknown = parser.parse_known_args()
     if opt.name and opt.resume:
