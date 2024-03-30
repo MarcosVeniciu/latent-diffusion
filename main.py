81b2821564c9c12425da414828f4d16a9edf7c83
Marcos Veniciu de sa Barbalho <marcos.barbalho@ufv.br>
2024-03-30 13:29:33 -0300

    Erro #1

--- main.py
+++ main.py
@@ -492,7 +492,7 @@ if __name__ == "__main__":
         #debug = parser.debug,
         #seed = parser.seed,
         #postfix = parser.postfix,
-        logdir = parser.logdir,
+        #logdir = parser.logdir,
         scale_lr = parser.scale_lr,
         gpus = parser.gpus,
         accelerator = parser.accelerator,
