ac5bc6c9c037066f186064b4c739d990305f1a59
Marcos Veniciu de sa Barbalho <marcos.barbalho@ufv.br>
2024-03-30 14:28:03 -0300

    Erro #1

--- main.py
+++ main.py
@@ -483,8 +483,8 @@ if __name__ == "__main__":
 
     parser = get_parser()
     #parser = parser.parse_args()
-    args_train = parser.__dict__
-    parser = Trainer(**args_train)
+    #args_train = parser.__dict__
+    #parser = Trainer(**args_train)
         #name = parser.name,
         #resume = parser.resume,
         #base = parser.base,
