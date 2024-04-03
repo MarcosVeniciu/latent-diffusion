bc5124e954900306417d30a6d88b1e93157b385b
Marcos Veniciu de sa Barbalho <marcos.barbalho@ufv.br>
2024-04-03 08:53:03 -0300

    Dataset

--- data/lsun/mover_imagens.py
+++ data/lsun/mover_imagens.py
@@ -5,6 +5,7 @@ base = "churches/imgs"
 
 def abrir_diretorio(dir):
 	for item in os.listdir(dir):
+		print(os.listdir(dir))
 		novo_dir = dir + "/" + item
 		print(item)
 		abrir_diretorio(novo_dir)
